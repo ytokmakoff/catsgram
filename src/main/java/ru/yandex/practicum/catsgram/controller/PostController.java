7940300fd78e492909835cd8620b531b0bf21a4f
ytokmakoff <ytokmakoff@gmail.com>
2024-02-16 10:19:05 +0300

    feat: add findbyId method

--- src/main/java/ru/yandex/practicum/catsgram/controller/PostController.java
+++ src/main/java/ru/yandex/practicum/catsgram/controller/PostController.java
@@ -1,14 +1,12 @@
 package ru.yandex.practicum.catsgram.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 import ru.yandex.practicum.catsgram.model.Post;
 import ru.yandex.practicum.catsgram.service.PostService;
 
 import java.util.List;
+import java.util.Optional;
 
 @RestController
 public class PostController {
@@ -27,5 +25,10 @@ public class PostController {
     @PostMapping(value = "/post")
     public Post create(@RequestBody Post post) {
         return postService.create(post);
+    }
+
+    @PostMapping("/posts/{postId}")
+    public Optional<Post> findById(@PathVariable int postId) {
+        return postService.findById(postId);
     }
 }
