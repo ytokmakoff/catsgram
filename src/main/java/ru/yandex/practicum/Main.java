3273469de6fb1a75d348c596a6a855effce167c6
ytokmakoff <ytokmakoff@gmail.com>
2024-05-09 17:15:15 +0300

    feat: add logging

--- src/main/java/ru/yandex/practicum/Main.java
+++ src/main/java/ru/yandex/practicum/Main.java
@@ -1,7 +1,18 @@
 package ru.yandex.practicum;
 
+import ch.qos.logback.classic.Level;
+import ch.qos.logback.classic.Logger;
+import org.slf4j.LoggerFactory;
+
 public class Main {
+
+    private static final Logger log = (ch.qos.logback.classic.Logger) LoggerFactory.getLogger(Main.class);
     public static void main(String[] args) {
-        System.out.println("catsgram");
+        log.setLevel(Level.WARN);
+        log.trace("Трасировочное сообщение");
+        log.debug("Отладочное сообщение");
+        log.info("Информационное сообщение");
+        log.warn("Предупреждающее сообщение");
+        log.error("Сообщение о критической ошибке");
     }
 }
