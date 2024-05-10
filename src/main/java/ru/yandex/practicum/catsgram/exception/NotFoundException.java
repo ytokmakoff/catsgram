6203518b244dc86ea28932b3fa1a7b6a0abce6b5
ytokmakoff <ytokmakoff@gmail.com>
2024-05-10 23:40:33 +0300

    feat: create NotFoundException

--- src/main/java/ru/yandex/practicum/catsgram/exception/NotFoundException.java
+++ src/main/java/ru/yandex/practicum/catsgram/exception/NotFoundException.java
@@ -1,2 +1,7 @@
-package ru.yandex.practicum.catsgram.exception;public class NotFoundException {
+package ru.yandex.practicum.catsgram.exception;
+
+public class NotFoundException extends RuntimeException {
+    public NotFoundException(String message) {
+        super(message);
+    }
 }
