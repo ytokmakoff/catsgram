b7c5ff1cbb367ec2809dce934e2349b70b7a9c76
ytokmakoff <ytokmakoff@gmail.com>
2024-05-10 23:35:46 +0300

    feat: create HomeController and PostController

--- src/main/java/ru/yandex/practicum/catsgram/controller/PostController.java
+++ src/main/java/ru/yandex/practicum/catsgram/controller/PostController.java
@@ -1,2 +1,44 @@
-package ru.yandex.practicum.catsgram.controller;public class PostController {
+package ru.yandex.practicum.catsgram.controller;
+
+
+import org.springframework.web.bind.annotation.*;
+import ru.yandex.practicum.catsgram.exception.ConditionsNotMetException;
+import ru.yandex.practicum.catsgram.model.Post;
+
+import java.time.Instant;
+import java.util.Collection;
+import java.util.HashMap;
+import java.util.Map;
+
+@RestController
+@RequestMapping("/posts")
+public class PostController {
+
+    private final Map<Long, Post> posts = new HashMap<>();
+
+    @GetMapping
+    public Collection<Post> findAll() {
+        return posts.values();
+    }
+
+    @PostMapping
+    public Post create(@RequestBody Post post) {
+        if (post.getDescription() == null || post.getDescription().isBlank()) {
+            throw new ConditionsNotMetException("Описание не может быть пустым");
+        }
+
+        post.setId(getNextId());
+        post.setPostDate(Instant.now());
+        posts.put(post.getId(), post);
+        return post;
+    }
+
+    private long getNextId() {
+        long currentMaxId = posts.keySet()
+                .stream()
+                .mapToLong(id -> id)
+                .max()
+                .orElse(0);
+        return ++currentMaxId;
+    }
 }
