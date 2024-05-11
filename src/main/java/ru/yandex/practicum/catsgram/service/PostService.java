57dfd161d61cbbbf0b8d855cbaa289119a868166
ytokmakoff <ytokmakoff@gmail.com>
2024-05-11 16:58:50 +0300

    feat: add arguments for findAll method

--- src/main/java/ru/yandex/practicum/catsgram/service/PostService.java
+++ src/main/java/ru/yandex/practicum/catsgram/service/PostService.java
@@ -8,26 +8,36 @@ import ru.yandex.practicum.catsgram.model.Post;
 import ru.yandex.practicum.catsgram.model.User;
 
 import java.util.ArrayList;
+import java.util.Comparator;
 import java.util.List;
+import java.util.stream.Collectors;
 
 @Service
 public class PostService {
+    private static Integer globalId = 0;
     private final UserService userService;
     private final List<Post> posts = new ArrayList<>();
-
-    private static Integer globalId = 0;
 
     @Autowired
     public PostService(UserService userService) {
         this.userService = userService;
     }
 
-    public List<Post> findAll() {
-        return posts;
+    private static Integer getNextId() {
+        return globalId++;
     }
 
-    private static Integer getNextId(){
-        return globalId++;
+    public List<Post> findAll(int from, int size, String sort) {
+
+        SortOrder order = SortOrder.from(sort);
+
+        return posts.stream()
+                .skip(from)
+                .limit(size)
+                .sorted(order.equals(SortOrder.ASCENDING) ?
+                        Comparator.comparing(Post::getCreationDate) :
+                        Comparator.comparing(Post::getCreationDate).reversed())
+                .collect(Collectors.toList());
     }
 
     public Post create(Post post) {
@@ -48,5 +58,16 @@ public class PostService {
                 .filter(p -> p.getId().equals(postId))
                 .findFirst()
                 .orElseThrow(() -> new PostNotFoundException(String.format("Пост № %d не найден", postId)));
+    }
+}
+
+enum SortOrder {
+    ASCENDING, DESCENDING;
+    public static SortOrder from (String order) {
+        return switch (order.toLowerCase()) {
+            case "ascending", "asc" -> ASCENDING;
+            case "descending", "desc" -> DESCENDING;
+            default -> null;
+        };
     }
 }
