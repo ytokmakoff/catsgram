adfaa6de1107782198063d4f37c7b9a2b8462a77
ytokmakoff <ytokmakoff@gmail.com>
2024-05-15 16:24:07 +0300

    feat: create ErrorResponse

--- src/main/java/ru/yandex/practicum/catsgram/controller/ErrorResponse.java
+++ src/main/java/ru/yandex/practicum/catsgram/controller/ErrorResponse.java
@@ -1,2 +1,13 @@
-package ru.yandex.practicum.catsgram.controller;public class ErrorResponse {
+package ru.yandex.practicum.catsgram.controller;
+
+public class ErrorResponse {
+    String error;
+
+    public ErrorResponse(String error) {
+        this.error = error;
+    }
+
+    public String getError() {
+        return error;
+    }
 }
