590fbf5d31f94c0a7ca91afbba6565079d1c9060
ytokmakoff <ytokmakoff@gmail.com>
2024-05-15 16:23:36 +0300

    refactor: add sort order instead of string check

--- src/main/java/ru/yandex/practicum/catsgram/controller/PostController.java
+++ src/main/java/ru/yandex/practicum/catsgram/controller/PostController.java
@@ -3,9 +3,12 @@ package ru.yandex.practicum.catsgram.controller;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
+import ru.yandex.practicum.catsgram.exception.ParameterNotValidException;
 import ru.yandex.practicum.catsgram.model.Post;
 import ru.yandex.practicum.catsgram.service.PostService;
+import ru.yandex.practicum.catsgram.service.SortOrder;
 
+import javax.swing.*;
 import java.util.List;
 
 @RestController
@@ -21,6 +24,15 @@ public class PostController {
     public List<Post> findAll(@RequestParam(defaultValue = "0") int from,
                               @RequestParam(defaultValue = "10") int size,
                               @RequestParam(defaultValue = "desc") String sort) {
+        SortOrder sortOrder = SortOrder.from(sort);
+        if (sortOrder == null)
+            throw new ParameterNotValidException("desc", "is not correct value, parameter must equals desc or asc");
+        if (size <= 0) {
+            throw new ParameterNotValidException("size", "size is must upper than zero");
+        }
+        if (from <= 0) {
+            throw new ParameterNotValidException("from", "from is must upper than zero");
+        }
         return postService.findAll(from, size, sort);
     }
 
