9580a73529a57c6a58a73d017e002d34e16779e8
ytokmakoff <ytokmakoff@gmail.com>
2024-05-15 16:21:10 +0300

    refactor: sortOrder rendered

--- src/main/java/ru/yandex/practicum/catsgram/service/SortOrder.java
+++ src/main/java/ru/yandex/practicum/catsgram/service/SortOrder.java
@@ -1,2 +1,13 @@
-package ru.yandex.practicum.catsgram.service;public enum SortOrder {
+package ru.yandex.practicum.catsgram.service;
+
+public enum SortOrder {
+    ASCENDING, DESCENDING;
+
+    public static SortOrder from(String order) {
+        return switch (order.toLowerCase()) {
+            case "ascending", "asc" -> ASCENDING;
+            case "descending", "desc" -> DESCENDING;
+            default -> null;
+        };
+    }
 }
