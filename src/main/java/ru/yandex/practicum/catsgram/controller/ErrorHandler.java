ad1af1773fa7485f58a3a1f7ac15cdd242ab87f6
ytokmakoff <ytokmakoff@gmail.com>
2024-05-15 16:28:22 +0300

    feat: create ErrorHandler

--- src/main/java/ru/yandex/practicum/catsgram/controller/ErrorHandler.java
+++ src/main/java/ru/yandex/practicum/catsgram/controller/ErrorHandler.java
@@ -1,2 +1,30 @@
-package ru.yandex.practicum.catsgram.controller;public class ErrorHandler {
+package ru.yandex.practicum.catsgram.controller;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+import ru.yandex.practicum.catsgram.exception.NotFoundException;
+import ru.yandex.practicum.catsgram.exception.ParameterNotValidException;
+
+@RestControllerAdvice
+public class ErrorHandler {
+    @ExceptionHandler
+    @ResponseStatus(HttpStatus.NOT_FOUND)
+    public ErrorResponse handleNotFoundException(NotFoundException e) {
+        return new ErrorResponse("not found", e.getMessage());
+    }
+
+    @ExceptionHandler
+    @ResponseStatus(HttpStatus.BAD_REQUEST)
+    public ErrorResponse handleParameterNotValidException(ParameterNotValidException e) {
+        return new ErrorResponse("Parameter not valid", e.getMessage());
+    }
+
+    @ExceptionHandler
+    @ResponseStatus(HttpStatus.INTERNAL_SERVER_ERROR)
+    public ErrorResponse handleThrowable(Throwable e) {
+        return new ErrorResponse("Произошла непредвиденная ошибка.", e.getMessage());
+    }
+
 }
