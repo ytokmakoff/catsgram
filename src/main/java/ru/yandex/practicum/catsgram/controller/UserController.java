59f95df32bf5ccc2ac717c9357ad00138e60e2dd
ytokmakoff <ytokmakoff@gmail.com>
2024-05-27 02:18:05 +0300

    feat: add RequestMapping(/users)

--- src/main/java/ru/yandex/practicum/catsgram/controller/UserController.java
+++ src/main/java/ru/yandex/practicum/catsgram/controller/UserController.java
@@ -2,7 +2,10 @@ package ru.yandex.practicum.catsgram.controller;
 
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.bind.annotation.RestController;
 import ru.yandex.practicum.catsgram.model.User;
 import ru.yandex.practicum.catsgram.service.UserService;
 
@@ -10,6 +13,7 @@ import java.util.List;
 
 @RestController
 @RequiredArgsConstructor
+@RequestMapping("/users")
 public class UserController {
     private final UserService userService;
 
@@ -18,5 +22,4 @@ public class UserController {
     public List<User> getUsers() {
         return userService.getUsers();
     }
-
 }
