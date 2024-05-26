7d7c5296c8b04cf5d565d864f0c3d73549518482
ytokmakoff <ytokmakoff@gmail.com>
2024-05-27 02:00:06 +0300

    refactor: data is now stored in database

--- src/main/java/ru/yandex/practicum/catsgram/service/UserService.java
+++ src/main/java/ru/yandex/practicum/catsgram/service/UserService.java
@@ -1,47 +1,20 @@
 package ru.yandex.practicum.catsgram.service;
 
 import org.springframework.stereotype.Service;
-import ru.yandex.practicum.catsgram.exception.InvalidEmailException;
-import ru.yandex.practicum.catsgram.exception.UserAlreadyExistException;
+import ru.yandex.practicum.catsgram.dal.UserRepository;
 import ru.yandex.practicum.catsgram.model.User;
 
-import java.util.Collection;
-import java.util.HashMap;
-import java.util.Map;
+import java.util.List;
 
 @Service
 public class UserService {
-    private final Map<String, User> users = new HashMap<>();
+    private final UserRepository userRepository;
 
-    public Collection<User> findAll() {
-        return users.values();
+    public UserService(UserRepository userRepository) {
+        this.userRepository = userRepository;
     }
 
-    public User createUser(User user) {
-        if (user.getEmail() == null || user.getEmail().isBlank()) {
-            throw new InvalidEmailException("Адрес электронной почты не может быть пустым.");
-        }
-        if (users.containsKey(user.getEmail())) {
-            throw new UserAlreadyExistException("Пользователь с электронной почтой " +
-                    user.getEmail() + " уже зарегистрирован.");
-        }
-        users.put(user.getEmail(), user);
-        return user;
-    }
-
-    public User updateUser(User user) {
-        if (user.getEmail() == null || user.getEmail().isBlank()) {
-            throw new InvalidEmailException("Адрес электронной почты не может быть пустым.");
-        }
-        users.put(user.getEmail(), user);
-
-        return user;
-    }
-
-    public User findUserByEmail(String email) {
-        if (email == null) {
-            return null;
-        }
-        return users.get(email);
+    public List<User> getUsers() {
+        return userRepository.findAll();
     }
 }
