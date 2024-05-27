fc24300c81b7ee7281d5450e1ce6c1edce58aded
ytokmakoff <ytokmakoff@gmail.com>
2024-05-27 16:57:14 +0300

    feat: create BaseRepository

---
+++ src/main/java/ru/yandex/practicum/catsgram/dal/BaseRepository.java
@@ -0,0 +1,28 @@
+package ru.yandex.practicum.catsgram.dal;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.dao.EmptyResultDataAccessException;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.jdbc.core.RowMapper;
+
+import java.util.List;
+import java.util.Optional;
+
+@RequiredArgsConstructor
+public class BaseRepository<T> {
+    protected final JdbcTemplate jdbc;
+    protected final RowMapper<T> mapper;
+
+    protected Optional<T> findOne(String query, Object... params) {
+        try {
+            T result = jdbc.queryForObject(query, mapper, params);
+            return Optional.ofNullable(result);
+        } catch (EmptyResultDataAccessException ignored) {
+            return Optional.empty();
+        }
+    }
+
+    protected List<T> findMany(String query, Object... params) {
+        return jdbc.query(query, mapper, params);
+    }
+}
