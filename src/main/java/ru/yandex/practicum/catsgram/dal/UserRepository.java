44a75e869f5e0b3052eca043a1dfc960a9bb8514
ytokmakoff <ytokmakoff@gmail.com>
2024-05-27 16:50:06 +0300

    feat: create findUserByEmail and findById methods

--- src/main/java/ru/yandex/practicum/catsgram/dal/UserRepository.java
+++ src/main/java/ru/yandex/practicum/catsgram/dal/UserRepository.java
@@ -1,12 +1,14 @@
 package ru.yandex.practicum.catsgram.dal;
 
 import lombok.RequiredArgsConstructor;
+import org.springframework.dao.EmptyResultDataAccessException;
 import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.stereotype.Repository;
 import ru.yandex.practicum.catsgram.dal.mappers.UserRowMapper;
 import ru.yandex.practicum.catsgram.model.User;
 
 import java.util.List;
+import java.util.Optional;
 
 @Repository
 @RequiredArgsConstructor
@@ -17,5 +19,25 @@ public class UserRepository {
     public List<User> findAll() {
         String query = "SELECT * FROM users";
         return jdbc.query(query, mapper);
+    }
+
+    public Optional<User> findUserByEmail(String email) {
+        String query = "SELECT * FROM users WHERE email = ?";
+        try {
+            User result = jdbc.queryForObject(query, mapper, email);
+            return Optional.ofNullable(result);
+        } catch (EmptyResultDataAccessException ignored) {
+            return Optional.empty();
+        }
+    }
+
+    public Optional<User> findById(long userId) {
+        String query = "SELECT * FROM users WHERE id = ?";
+        try {
+            User result = jdbc.queryForObject(query, mapper, userId);
+            return Optional.ofNullable(result);
+        } catch (EmptyResultDataAccessException ignored) {
+            return Optional.empty();
+        }
     }
 }
